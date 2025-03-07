2428dbab62372265175c05032be38b78fac664bb
Hossein-Asadi <hosseinasd0109@gmail.com>
2025-03-07 17:28:57 +0330

    update input

--- src/components/Common/Button/Input.tsx
+++ src/components/Common/Button/Input.tsx
@@ -1,7 +1,6 @@
 import AlertIcon from "../icons/AlertIcon";
 import SuccessIcon from "../icons/SuccessIcon";
 
-
 interface InputFieldProps {
   type: string;
   value: string;
@@ -43,19 +42,20 @@ export default function InputField({ ...props }: InputFieldProps) {
 
   return (
     <div className="flex flex-col w-full relative">
-      <label htmlFor="input" className="text-sm font-light text-gray-500 mb-1">
-        {props.label}
-      </label>
-      <input
-        type={props.type}
-        className={`p-3 text-sm rounded-lg bg-gray-100 text-gray-600 w-full h-14 focus:outline-none focus:border-blue-400 ${borderClass}`}
-        placeholder={props.placeholder}
-        value={props.value}
-        name="input"
-        required={props.required}
-        dir={props.textDirection}
-        onChange={props.inputChangeHandler}
-      />
+      <div className="relative">
+        <input
+          type={props.type}
+          id="floating_outlined"
+          className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder={props.placeholder}
+          onChange={props.inputChangeHandler}
+          dir={props.textDirection}
+          required={props.required}
+        />
+        <label className="absolute top-0 right-4 peer-focus:block text-sm text-gray-500 dark:text-gray-400 duration-300 scale-75 z-10 origin-[0] bg-white dark:bg-gray-900 peer-focus:px-1 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 start-0.5">
+          {props.label}
+        </label>
+      </div>
       {renderInputSubtitle()}
     </div>
   );
