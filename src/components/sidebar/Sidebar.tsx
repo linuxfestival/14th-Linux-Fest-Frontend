647b7eaa7874e5dc76cfea77375f88331b656e55
Hossein-Asadi <hosseinasd0109@gmail.com>
2025-03-07 20:33:33 +0330

    Update settings section

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { useCallback } from "react";
 import { sidebarData } from "../../assets/constants";
 import SidebarButton from "./SidebarButton";
 import Logout from "../Common/icons/Logout";
+import SidebarLogo from "../Common/icons/SidebarLogo";
 
 const Sidebar = () => {
   const handleButtonClick = useCallback((action: string) => {
@@ -9,24 +10,28 @@ const Sidebar = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-stretch bg-gray-900 text-white p-5 w-64 space-y-4 h-full">
-      {sidebarData.map((button, index) => (
-        <SidebarButton
-          key={index}
-          label={button.label}
-          icon={button.icon}
-          onClick={() => handleButtonClick(button.path)}
-          className={button.colorClass}
-        />
-      ))}
-      <div>
-        <SidebarButton
-          label="Siktir"
-          onClick={() => handleButtonClick("test")}
-          icon={<Logout />}
-          className="bg-[#9B2C3B] hover:bg-[#7a1e2b]"
-        />
+    <div className="flex flex-col justify-between bg-bg-sidebar text-white p-5 w-64 space-y-4 h-[100vh]">
+      <div className="flex flex-col items-center gap-[10px]">
+        <div className="flex w-full justify-between items-center">
+          <SidebarLogo />
+          <p className="font-bold text-xl">لینوکس فست</p>
+        </div>
+        {sidebarData.map((button, index) => (
+          <SidebarButton
+            key={index}
+            label={button.label}
+            icon={button.icon}
+            onClick={() => handleButtonClick(button.path)}
+            className="hover:bg-secondary"
+          />
+        ))}
       </div>
+      <SidebarButton
+        label="Siktir"
+        onClick={() => handleButtonClick("test")}
+        icon={<Logout />}
+        className="bg-secondary hover:bg-[#7a1e2b]"
+      />
     </div>
   );
 };
